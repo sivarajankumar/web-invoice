d9ea67656f4aee6def43df66cb917a2bfecc5e20
sh.mohanjith <sh.mohanjith@a6b3a22d-3c21-0b44-ec54-48342a2ab320>
2010-08-13 15:26:27 +0000

    * Fixed issue 40: Web Template Content not working 100%

--- web-invoice.php
+++ web-invoice.php
@@ -4,7 +4,7 @@
  Plugin URI: http://mohanjith.com/wordpress/web-invoice.html
  Description: Send itemized web invoices directly to your clients.  Credit card payments may be accepted via Authorize.net, MerchantPlus NaviGate, Moneybookers, AlertPay, Google Checkout or PayPal account. Recurring billing is also available via Authorize.net's ARB, Moneybookers, Google Checkout and PayPal. Visit <a href="admin.php?page=web_invoice_settings">Web Invoice Settings Page</a> to setup.
  Author: S H Mohanjith
- Version: 2.0.12
+ Version: 2.0.13
  Author URI: http://mohanjith.com/
  Text Domain: web-invoice
  Stable tag: 2.0.12
